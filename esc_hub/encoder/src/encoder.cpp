ce92e1a098523f6b078cc9cae402ece6bb7622ae
kanai0914 <ayuayugonbe@gmail.com>
2026-06-15 22:47:02 +0900

    feat:can通信できるプログラム追加

--- esc_hub/encoder/src/encoder.cpp
+++ esc_hub/encoder/src/encoder.cpp
@@ -10,45 +10,51 @@
 #include "tim.h"
 
 gn10_can::drivers::FDCANDriver fdcan1_driver(&hfdcan1);
-gn10_can::drivers::FDCANDriver fdcan2_driver(&hfdcan2);
+VescCAN vesc(&hfdcan2);
 
 gn10_can::FDCANBus fdcan1_bus(fdcan1_driver);
-gn10_can::FDCANBus fdcan2_bus(fdcan2_driver);
 gn10_can::devices::ESCHubServer esc_hub(fdcan1_bus, 0);
-gn10_can::devices::ESCHubServer esc_hub_2(fdcan2_bus, 0);
 
 int16_t read_encoder_value(void)
 {
     uint16_t enc_buff = TIM8->CNT;
     TIM8->CNT         = 0;
-    if (enc_buff > 32767) {
-        return (int16_t)enc_buff * -1;
-    } else {
-        return (int16_t)enc_buff;
-    }
+
+    return (int16_t)enc_buff;
 }
 
 void setup()
 {
     HAL_TIM_Encoder_Start(&htim8, TIM_CHANNEL_ALL);
+    vesc.init();
     fdcan1_driver.init();
-    fdcan2_driver.init();
+    // 原点取り
+
     while (HAL_GPIO_ReadPin(LIM1_2_GPIO_Port, LIM1_2_Pin) == GPIO_PIN_SET) {
-        esc_hub.set_encoder_feedbacks(1231);
-        esc_hub_2.set_encoder_feedbacks(1232);
-        HAL_GPIO_TogglePin(LED_2_GPIO_Port, LED_2_Pin);
+        vesc.comm_can_set_current(45, -0.5f);
+        vesc.comm_can_set_duty(45, -0.5f);
 
-        HAL_Delay(1);
+        HAL_GPIO_TogglePin(LED_3_GPIO_Port, LED_3_Pin);
     }
-    HAL_GPIO_TogglePin(LED_3_GPIO_Port, LED_3_Pin);
-}
 
-void loop() {}
+    TIM8->CNT = 0;
+}
+static int32_t enc_buff = 123;
+
+void loop()
+{
+    HAL_GPIO_TogglePin(LED_2_GPIO_Port, LED_2_Pin);  // ← 先頭に追加
+
+    esc_hub.set_encoder_feedbacks(enc_buff);
+
+    // loop()内のエラーチェック部分を変更
+
+    HAL_Delay(1);
+}
 
 void HAL_FDCAN_RxFifo0Callback(FDCAN_HandleTypeDef* hfdcan, uint32_t RxFifo0ITs)
 {
     if (hfdcan->Instance == hfdcan1.Instance) {
         fdcan1_bus.update();
-        HAL_GPIO_TogglePin(LED_4_GPIO_Port, LED_4_Pin);
     }
 }
