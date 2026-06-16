55a4c8aed8c1520e7c8f5faf5a8352cc78443c13
kanai0914 <ayuayugonbe@gmail.com>
2026-06-16 16:26:41 +0900

    fix:USBXを削除することによってcan通信を安定して動作させるようにした

--- esc_hub/encoder/src/encoder.cpp
+++ esc_hub/encoder/src/encoder.cpp
@@ -15,6 +15,22 @@ VescCAN vesc(&hfdcan2);
 gn10_can::FDCANBus fdcan1_bus(fdcan1_driver);
 gn10_can::devices::ESCHubServer esc_hub(fdcan1_bus, 0);
 
+constexpr uint32_t k_heartbeat_toggle_interval_ms = 500;
+
+uint32_t heartbeat_last_toggle_time_ms = 0;
+
+/**
+ * @brief Toggle heartbeat LED at a fixed interval.
+ */
+void update_heartbeat_led()
+{
+    const uint32_t now_ms = HAL_GetTick();
+    if ((now_ms - heartbeat_last_toggle_time_ms) >= k_heartbeat_toggle_interval_ms) {
+        heartbeat_last_toggle_time_ms = now_ms;
+        HAL_GPIO_TogglePin(LED_4_GPIO_Port, LED_4_Pin);
+    }
+}
+
 int16_t read_encoder_value(void)
 {
     uint16_t enc_buff = TIM8->CNT;
@@ -34,17 +50,16 @@ void setup()
         vesc.comm_can_set_current(45, -0.5f);
         vesc.comm_can_set_duty(45, -0.5f);
 
-        HAL_GPIO_TogglePin(LED_3_GPIO_Port, LED_3_Pin);
+        HAL_GPIO_WritePin(LED_1_GPIO_Port, LED_1_Pin, GPIO_PIN_SET);
     }
-
+    HAL_GPIO_WritePin(LED_1_GPIO_Port, LED_1_Pin, GPIO_PIN_RESET);
     TIM8->CNT = 0;
 }
 static int32_t enc_buff = 123;
 
 void loop()
 {
-    HAL_GPIO_TogglePin(LED_2_GPIO_Port, LED_2_Pin);  // ← 先頭に追加
-
+    update_heartbeat_led();
     esc_hub.set_encoder_feedbacks(enc_buff);
 
     // loop()内のエラーチェック部分を変更
